6abadda04a89108d29ee162b68a87f84b697fc66
ESTHERAJUOGU <135168720+ESTHERAJUOGU@users.noreply.github.com>
2023-09-24 15:53:42 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import axios from "axios";
+let apiKey = b3b7f0a3ff1dbcc14114dfae05acef2f;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&unit=metric`;
+console.log("apiUrl");
 function formatDate(timeStamp) {
+    let date=new Date(timeStamp);
+    let hours=timeStamp.getHours();
+    let minutes=date.getMinutes();
+    if (minutes<10){
+        let hours=date.getHours();
+        if(hours<10){
+            hours=`0${hours}`;
+        }
+    }
+    let days=["Sunday","Monday","Tueaday","Wednesday","Thursday","Friday","Saturday"]
+    let day=days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
     
 }
 function displayTemperature(response){
@@ -10,5 +25,12 @@ function displayTemperature(response){
     cityElement.innerHTML=response.data.name;
     let descriptionElement = document.querySelector("#description");
 descriptionElement.innerHTML=respons.data.weather[0].description;
-
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = respons.data.main.humidity;
+let windElement = document.querySelector("#wind");
+windElement.innerHTML = Math.round(respons.data.wind.speed);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(response.data.dt*1000);
+let iconElement = document.querySelector("#icon");
+iconElement.innerHTML = `https:/openweathermap.org/img/wn/04d@2x.png`;
 }
