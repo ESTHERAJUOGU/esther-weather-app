d85ccc3311edd35df73e20d6f3b7e89d5831d107
ESTHERAJUOGU <135168720+ESTHERAJUOGU@users.noreply.github.com>
2023-09-24 15:18:00 +0100

    updated index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function displayTemperature(response){
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML=response.data.name;
-    
+    let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML=respons.data.weather[0].description;
 
 }
